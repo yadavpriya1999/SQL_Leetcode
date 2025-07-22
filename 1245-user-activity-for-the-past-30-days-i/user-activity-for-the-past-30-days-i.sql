dc4d329e939a3751db533287c4bc4b960347b2f5
PRIYA YADAV <86567092+yadavpriya1999@users.noreply.github.com>
2025-07-22 22:19:47 +0530

    Time: 700 ms (7.28%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -3,5 +3,5 @@ select
  activity_date as day,
  count(distinct user_id) as active_users
 from Activity
-where  activity_date >= '2019-06-28'  and activity_date <= '2019-07-27'
+where  datediff('2019-07-27',activity_date)<30  and activity_date <= '2019-07-27'
 group by activity_date;
