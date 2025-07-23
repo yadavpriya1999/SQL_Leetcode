5ac5d3c7b4740e032f829ee2f8e656ba34328c81
PRIYA YADAV <86567092+yadavpriya1999@users.noreply.github.com>
2025-07-23 19:59:17 +0530

    Time: 282 ms (39.63%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-select max(salary) as  SecondHighestSalary
-from Employee e1
-where 1 = (select count(distinct salary)
-from Employee e2 where e2.salary > e1.salary);
+select max(salary) as SecondHighestSalary
+from Employee
+where salary < (
+    select max(salary)
+    from Employee
+);
