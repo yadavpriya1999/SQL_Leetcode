64fc119f0764e1eea6f557b12d91e343ce340337
PRIYA YADAV <86567092+yadavpriya1999@users.noreply.github.com>
2025-07-25 12:02:32 +0530

    Time: 809 ms (23.26%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,11 +1,8 @@
-# Write your MySQL query statement below
 
-with cte as
-(select p.product_name,sum(o.unit) as unit
+select p.product_name,sum(unit) as unit
 from Products p
-join Orders o on p.product_id =o.product_id 
-where month(o.order_date) = 2 and year(o.order_date)=2020
-group by p.product_name )
-
-select * from cte
-where unit >= 100;
+inner join Orders o
+on p.product_id=o.product_id
+where month(o.order_date)=2 and year(o.order_date)=2020
+group by p.product_name
+having sum(unit) >= 100;
