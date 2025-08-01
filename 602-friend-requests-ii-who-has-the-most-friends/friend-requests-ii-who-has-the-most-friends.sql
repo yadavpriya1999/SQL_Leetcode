79a688830cd05bb019ab6a166fcd95420e30d1d7
PRIYA YADAV <86567092+yadavpriya1999@users.noreply.github.com>
2025-08-01 13:30:07 +0530

    Time: 312 ms (47.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+with cte as (select requester_id as id ,accepter_id
+from RequestAccepted 
+union
+select  accepter_id as id ,requester_id
+from RequestAccepted)
+
+select id, count(accepter_id) as num
+from cte
+group by id
+order by num desc
+limit 1;
